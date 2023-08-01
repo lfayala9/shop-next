92bd994a692e5719720f0c00d434a54d74d8ba34
Luis Felipe <lfayala9@gmail.com>
2023-08-01 19:46:45 -0400

    form validation added and vercel.json

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -2,6 +2,7 @@ import Layout from "@containers/Layout";
 import SignForm from "@containers/SignForm";
 
 const SignUp = () =>{
+    console.log(process.env.NEXT_PUBLIC_API_URL)
     return(
         <Layout authPage={true}>
             <SignForm />
